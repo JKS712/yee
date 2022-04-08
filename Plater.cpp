e5497251477cfa69cd3076468936a911fb2e6680
cosmo3781193 <101332423+cosmo3781193@users.noreply.github.com>
2022-04-08 21:42:04 +0800

    未完成

---
+++ Plater.cpp
@@ -0,0 +1,27 @@
+#include <Player.h>
+
+Player::Player(double x, double y)
+{
+    this->x = x;
+    this->y = y;
+}
+double Player::X()
+{
+    return x;
+}
+double Player::Y()
+{
+    return y;
+}
+void Player::Draw()
+{
+
+}
+void Player::Erase()
+{
+
+}
+void Player::Move()
+{
+
+}

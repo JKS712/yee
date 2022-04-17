3f0ac9534dca3f3244da99e24b69a60e6f353cd6
cosmo3781193 <101332423+cosmo3781193@users.noreply.github.com>
2022-04-17 21:57:03 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,14 @@
 #include <Player.h>
+#include <conio.h>
+#define KEY_UP    72
+#define KEY_DOWN  80
+#define KEY_LEFT  75
+#define KEY_RIGHT 77
+#define BORDER_UP 2
+#define BORDER_DOWN 28
+#define BORDER_LEFT 43 
+#define BORDER_RIGHT 73
+#define SPEED_PLAYER 1.1
 
 Player::Player(double x, double y)
 {
@@ -25,5 +35,37 @@ void Player::Erase()
 }
 void Player::Move()
 {
-
+    if(kbhit())
+    {
+        Erase();
+        
+        char key = getch();
+        switch(key)
+        {
+            case KEY_LEFT: 
+                if(x - SPEED_PLAYER > BORDER_LEFT)
+                { 
+                    x -= SPEED_PLAYER; 
+                    break; 
+                }
+            case KEY_RIGHT: 
+                if (x + SPEED_PLAYER < BORDER_RIGHT) 
+                {
+                    x += SPEED_PLAYER; 
+                    break; 
+                }
+            case KEY_UP: 
+                if (y - SPEED_PLAYER > BORDER_UP) 
+                { 
+                    y -= SPEED_PLAYER; 
+                    break;
+                }
+            case KEY_DOWN: 
+                if (y + SPEED_PLAYER < BORDER_DOWN) 
+                { 
+                    y += SPEED_PLAYER; 
+                    break; 
+                } 
+        }
+    }
 }

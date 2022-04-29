d39e7ba73646c161032932e99f25c242dafe51d4
lilblak <101306549+lilblak@users.noreply.github.com>
2022-04-29 13:16:33 +0800

    Update attack.h

--- attack.h
+++ attack.h
@@ -3,14 +3,19 @@
 
 class attack{
 public:
+	arrack(int);
 	int getSize();
 	void setSize(int,int);
 	int getDamage();
 	void setDamage(int);
 	int getAttackSpeed();
 	void setAttackSpeed(int);
+	void printAttack();
+	void eraseAttack();
 private:
 	int Damage;
+	double x;
+	double y;
 };
 
 #endif

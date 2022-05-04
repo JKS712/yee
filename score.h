8461cf93b234f299b2529a3833ce980f5d5a7be2
lilblak <101306549+lilblak@users.noreply.github.com>
2022-05-04 09:28:24 +0800

    Add files via upload

---
+++ score.h
@@ -0,0 +1,18 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+class score{
+public:
+	score();
+	void setScore(int);
+	void printScore();
+	void eraseScore();
+
+private:
+	int score;
+	double X;
+	double Y;
+
+};
+
+#endif

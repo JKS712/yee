a3035ade39d9c3e235f41a84a7f950cc3a9be05a
cosmo3781193 <101332423+cosmo3781193@users.noreply.github.com>
2022-06-05 22:18:14 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,20 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-
 class Player
 {
-private:
-    double x;
-    double y;
 public:
     Player(double x, double y);
     double X();
     double Y();
-    void Draw();
-    void Erase();
-    void printAttack();
-    void eraseAttack();
-    void Move();
+    void PlayerDraw();
+    void PlayerErase();
+    void PlayerMove();
+private:
+    double x; 
+    double y;
 };
 
-#endif
+#endif 

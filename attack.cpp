6ce26a1d087aaa63c1c27017d371201e01727945
cosmo3781193 <101332423+cosmo3781193@users.noreply.github.com>
2022-06-05 22:19:55 +0800

    Update attack.cpp

--- attack.cpp
+++ attack.cpp
@@ -1,34 +1,54 @@
+#include"Attack.h"
 #include<iostream>
-#include"attack.h"
-	
-attack::attack(){
-	int attack = 1;
+#include<Windows.h>
+#define SPEED_ATTACK 0.5
+#define BORDER_UP 2
+using namespace std;
+
+Attack::Attack(double x, double y)
+{
+    this->x = x;
+    this->y = y;
+}
+double Attack::X()
+{
+    return x;
+}
+double Attack::Y()
+{
+    return y;
+}
+void Attack::AttackDraw()
+{
+    gotoxy(x, y); cout << "l"; //  while (y - 1) means a row above y
+
 }
 
-int attack::getSize(){
-	return size;
-}
-void attack::setSize(int width,int height){
-	size= width*height;
-}
-int attack::getDamage(){
-	return damage;
-}
-void attack::setDamage(int newDamage){
-	damage= newDamage;
-}
-int attack::getAttackSpeed(){
-	return attackSpeed;
-}
-void attack::setAttackSpeed(int newAttackSpeed){
-	attackSpeed=newAttackSpeed;
+void Attack::AttackErase()
+{
+    gotoxy(x, y); cout << "  ";
 }
 
-void attack::printAttack() {
-	gotoxy(x, y); cout << "KKK";
-	gotoxy(x, y + 1); cout << " K ";
+void Attack::AttackMove()
+{
+    AttackErase();
+    y -= SPEED_ATTACK; // move a SPEED_PASS unit upward
+    AttackDraw();
 }
-void attack::eraseAttack() {
-	gotoxy(x, y); cout << "  ";
-	gotoxy(x, y + 1); cout << "  ";
+
+bool Attack::AttackisOut()
+{
+    if (y < BORDER_UP)
+        return true;
+    else
+        return false;
+}
+
+void gotoxy(double x, double y)
+{
+    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = x; // start from 0
+    dwPos.Y = y; // start from 0
+    SetConsoleCursorPosition(hCon, dwPos);
 }
